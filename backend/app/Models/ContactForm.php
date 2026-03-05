79cf842299f55d6d4130bd05d8d0ab726bb9eb2a
roro333111 <arturbartres@gmail.com>
2026-03-05 12:26:47 +0100

    models, falten relacions

---
+++ backend/app/Models/ContactForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContactForm extends Model
+{
+    protected $table = "contact_forms";
+    protected $fillable = ['user_id','user_email','user_phone','user_issue','message'];
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
