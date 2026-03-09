0e5a221fef6e5507929fa1fd68ab2d4f8dcf2e56
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2026-03-09 20:36:00 +0100

    routes products

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+
 Route::get('/test', function () {
     return response()->json([
         'message' => 'API funcionando'
