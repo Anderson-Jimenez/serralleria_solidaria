f981b762c22c14c3b307141edf4cada6f518a561
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2026-03-09 10:56:57 +0100

    categories

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/test', function () {
     return response()->json([
         'message' => 'API funcionando'
     ]);
 });
+
+Route::get('/categories', [CategoryController::class, 'index']);
