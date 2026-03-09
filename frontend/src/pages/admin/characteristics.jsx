bfcf3165d66b9333af303f3adc13f680f143a34c
roro333111 <arturbartres@gmail.com>
2026-03-09 10:57:14 +0100

    vista caracteristiques

--- frontend/src/pages/admin/characteristics.jsx
+++ frontend/src/pages/admin/characteristics.jsx
@@ -1,29 +1,63 @@
 function Characteristics() {
   return (
-    <div className="dashboard-content table-container">
-      <table>
-        <thead>
-          <tr>
-            <th><input type="checkbox"/></th>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Descripció</th>
-            <th>Estat</th>
-            <th>Accions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><input type="checkbox"/></td>
-            <td>18233</td>
-            <td>Bombins</td>
-            <td class="description">Lorem Ipsum is simply dummy text...</td>
-            <td>Actiu</td>
-            <td class="actions">
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="dashboard-content">
+      <div className="caracteristics-content">
+        <div className="table-container">
+          <table>
+            <thead>
+              
+              <tr className="tableFilters">
+                <td colSpan={2}> <input type="text" name="" id="" /> </td>
+                <td> 
+                  <select name="" id="">
+
+                  </select> 
+                </td>
+                <td><button>Afegir Seguretat +</button></td>
+              </tr>
+
+              <tr>
+                <th><input type="checkbox" /></th>
+                <th>ID</th>
+                <th>Seguretat</th>
+                <th>--</th>
+              </tr>
+              
+            </thead>
+            <tbody>
+
+              <tr>
+                <td><input type="checkbox" /></td>
+                <td>18233</td>
+                <td>Segurrro</td>
+                <td></td>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th><input type="checkbox" /></th>
+                <th>ID</th>
+                <th>Tipus</th>
+                <th>ID Caracteristica</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><input type="checkbox" /></td>
+                <td>18233</td>
+                <td>Bombins</td>
+                <td>1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
     </div>
   );
 }
