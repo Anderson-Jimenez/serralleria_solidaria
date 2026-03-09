6a1884739a0b745f25adc9b154e6f54a1249ca6f
roro333111 <arturbartres@gmail.com>
2026-03-09 10:29:24 +0100

    base de la taula

--- frontend/src/pages/admin/characteristics.jsx
+++ frontend/src/pages/admin/characteristics.jsx
@@ -1,8 +1,29 @@
 function Characteristics() {
   return (
-    <div className="dashboard-content">
-      <h2 style={{ color: "#444", fontSize: "24px" }}>Aquestes son les característiques</h2>
-
+    <div className="dashboard-content table-container">
+      <table>
+        <thead>
+          <tr>
+            <th><input type="checkbox"/></th>
+            <th>ID</th>
+            <th>Nom</th>
+            <th>Descripció</th>
+            <th>Estat</th>
+            <th>Accions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><input type="checkbox"/></td>
+            <td>18233</td>
+            <td>Bombins</td>
+            <td class="description">Lorem Ipsum is simply dummy text...</td>
+            <td>Actiu</td>
+            <td class="actions">
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
