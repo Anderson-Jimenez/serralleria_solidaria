f4f078a4d5ac415af25db99b2aadb007d277c5d4
roro333111 <arturbartres@gmail.com>
2026-04-15 13:27:32 +0200

    proba funciona

--- frontend/src/pages/admin/loginAdmin.jsx
+++ frontend/src/pages/admin/loginAdmin.jsx
@@ -2,27 +2,36 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Search, Plus, Pencil, Power, Trash2 } from "lucide-react";
 
+import Threads from "../../components/bg/threads";
+
 function LoginAdmin() {
 
   return (
     <section className="loginSection">
+      <div className="threads-container">
+        <Threads
+          amplitude={5}
+          distance={0}
+          enableMouseInteraction={false}
+        />
+      </div>
       <div className="loginContainer">
         <h1>Login</h1>
         <form action="">
           <div>
             <label htmlFor="username">Usuari: </label>
-            <input type="username" /> 
+            <input type="username" />
           </div>
           <div>
             <label htmlFor="password">Contrasenya: </label>
-            <input type="password" /> 
+            <input type="password" />
           </div>
           <div>
             <a href="#">Has oblidat la contrasenya?</a>
           </div>
         </form>
       </div>
-    </section>  
+    </section>
   );
 }
 
